edfa03fdc786ee47b9d9066f1a9a2739c45880f2
nishizawa-ryunosuke <ryukuu0919@gmail.com>
2023-10-15 22:49:39 +0900

    現在のターンのボタンの色変更

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+// マスの要素を作成
+function Square({ value, onSquareClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+// ボードの要素を作成
+function Board({ xIsNext, squares, onPlay }) {
+  //
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-table">
+        <div className="board-row">
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+// gameを作成
+export default function Game() {
+  // ゲームの履歴を管理する
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  // 現在の手番を示す
+  const [currentMove, setCurrentMove] = useState(0);
+  // 現在のプレーヤーがXかOかを判定
+  const xIsNext = currentMove % 2 === 0;
+  // 現在の盤面の状態
+  const currentSquares = history[currentMove];
+
+  // ゲームの新しい手番の盤面の状態を更新し、その新しい状態を履歴に保存
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  // 特定の手番にジャンプする
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  // history配列をマップし、それぞれの手番に対するジャンプボタンを作成
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    // 現在のターンに該当するボタンに 'current-move' クラスを追加
+    let buttonClass;
+    if (move === currentMove) {
+      buttonClass = 'game-info-btn current-move'
+    } else {
+      buttonClass = 'game-info-btn';
+    }
+    return (
+      <li className="game-info-item" key={move}>
+        <button className={buttonClass} onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol className="game-info-list">{moves}</ol>
+      </div>
     </div>
   );
 }
 
-export default App;
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
