19442cb14003a176f8719cfbb80ad1e6d27f45d3
nishizawa-ryunosuke <ryukuu0919@gmail.com>
2023-10-16 17:21:47 +0900

    ループ処理実装

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import './App.css';
 
-// マスの要素を作成
 function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
@@ -10,19 +9,13 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-// ボードの要素を作成
 function Board({ xIsNext, squares, onPlay }) {
-  //
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
+    nextSquares[i] = xIsNext ? 'X' : 'O';
     onPlay(nextSquares);
   }
 
@@ -34,71 +27,58 @@ function Board({ xIsNext, squares, onPlay }) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
+  const boardRows = [];
+  for (let row = 0; row < 3; row++) {
+    const squaresInRow = [];
+    for (let col = 0; col < 3; col++) {
+      const squareIndex = row * 3 + col;
+      squaresInRow.push(
+        <Square
+          key={squareIndex}
+          value={squares[squareIndex]}
+          onSquareClick={() => handleClick(squareIndex)}
+        />
+      );
+    }
+    boardRows.push(
+      <div key={row} className="board-row">
+        {squaresInRow}
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-table">
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
-      </div>
+      <div className="board-table">{boardRows}</div>
     </>
   );
 }
 
-// gameを作成
 export default function Game() {
-  // ゲームの履歴を管理する
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  // 現在の手番を示す
   const [currentMove, setCurrentMove] = useState(0);
-  // 現在のプレーヤーがXかOかを判定
   const xIsNext = currentMove % 2 === 0;
-  // 現在の盤面の状態
   const currentSquares = history[currentMove];
 
-  // ゲームの新しい手番の盤面の状態を更新し、その新しい状態を履歴に保存
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
 
-  // 特定の手番にジャンプする
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
   }
 
-  // history配列をマップし、それぞれの手番に対するジャンプボタンを作成
   const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
-    // 現在のターンに該当するボタンに 'current-move' クラスを追加
-    let buttonClass;
-    if (move === currentMove) {
-      buttonClass = 'game-info-btn current-move'
-    } else {
-      buttonClass = 'game-info-btn';
-    }
+    const description = move > 0 ? 'Go to move #' + move : 'Go to game start';
+    const buttonClass = move === currentMove ? 'game-info-btn current-move' : 'game-info-btn';
     return (
       <li className="game-info-item" key={move}>
-        <button className={buttonClass} onClick={() => jumpTo(move)}>{description}</button>
+        <button className={buttonClass} onClick={() => jumpTo(move)}>
+          {description}
+        </button>
       </li>
     );
   });
